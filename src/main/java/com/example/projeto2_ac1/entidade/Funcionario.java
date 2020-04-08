0d369d870e81c830a29cff385453512fcc533f74
pedrovitro <pedrovitro@hotmail.com>
2020-04-07 22:01:04 -0300

    Create Funcionario.java
    Criação da Classe Entidade Funcionário

---
+++ src/main/java/com/example/projeto2_ac1/entidade/Funcionario.java
@@ -0,0 +1,70 @@
+package com.example.projeto2_ac1.entidade;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Funcionario implements Serializable{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    @Id
+    private int Id;
+    private double salario;
+    private String nome;
+    private String cargo;
+    private String sexo;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario [Id=" + Id + ", cargo=" + cargo + ", nome=" + nome + ", salario=" + salario + ", sexo="
+                + sexo + "]";
+    }
+
+    
+
+}
