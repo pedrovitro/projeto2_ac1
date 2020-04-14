9a82ba0e094886b94c39d8fc191929f0c359ad81
pedrovitro <pedrovitro@hotmail.com>
2020-04-14 15:39:22 -0300

    repository

---
+++ src/main/java/com/example/projeto2_ac1/repository/FuncionarioRepository.java
@@ -0,0 +1,11 @@
+package com.example.projeto2_ac1.repository;
+
+import com.example.projeto2_ac1.entidade.Funcionario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository <Funcionario, Integer>{
+
+
+    
+}
